a50a8696dca9089c341d185a3d487e4b0155ddcc
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-07-04 11:43:20 +0930

    fix: use .js for imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { App } from './decorators/app';
-export { routesAuth } from './decorators/auth';
-export { routeLog } from './decorators/log';
-export { Route } from './decorators/route';
-export * as internals from './internals';
+export { App } from './decorators/app.js';
+export { routesAuth } from './decorators/auth.js';
+export { routeLog } from './decorators/log.js';
+export { Route } from './decorators/route.js';
+export * as internals from './internals.js';
