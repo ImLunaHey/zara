f0232e3408779cbf01c56f96e8fe54e7f6be215e
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-07-04 11:45:17 +0930

    fix: remove WIP decorators from lib export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { App } from './decorators/app.js';
-export { routesAuth } from './decorators/auth.js';
-export { routeLog } from './decorators/log.js';
 export { Route } from './decorators/route.js';
 export * as internals from './internals.js';
