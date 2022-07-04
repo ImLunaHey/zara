c930f53c428a3514ae88268c4585f4be81f656dc
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-07-04 11:41:01 +0930

    feat: add lib entry point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { App } from './decorators/app';
+export { routesAuth } from './decorators/auth';
+export { routeLog } from './decorators/log';
+export { Route } from './decorators/route';
+export * as internals from './internals';
